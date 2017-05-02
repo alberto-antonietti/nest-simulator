3236ad895484ad68a3055201d1ed3853504bf664
Alberto <alberto.antonietti@polimi.it>
2017-05-02 20:21:22 +0200

    missing bracket

--- models/iaf_cond_exp.h
+++ models/iaf_cond_exp.h
@@ -294,7 +294,6 @@ iaf_cond_exp::handles_test_event( SpikeEvent&, rport receptor_type )
 {
   if ( receptor_type > 1 )
     throw UnknownReceptorType( receptor_type, get_name() );
-  }
   return 0;
 }
 
